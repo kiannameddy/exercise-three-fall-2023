b8e839e59dbccec7cb9b28a8969b68f9ae259505
kiannaeddy <kme9504@nyu.edu>
2023-10-03 09:48:23 -0400

    adding files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
-import "../app/globals.css" 
+import "../app/globals.css";
+import WeatherCard from "@/app/components/WeatherCard";
+
 
 export async function getStaticProps() {
     const res = await fetch (
@@ -16,6 +18,17 @@ export default function Home({weatherData}) {
     return (
         <main>
             <h1>Weather App</h1>
+            <WeatherCard 
+            cityName = {weatherData.name} 
+            weatherType = {weatherData.weather[0].main}
+            // currentTemp = {weatherData}
+            // highTemp = {weatherData}
+            // lowTemp = {weatherData}
+            // cloudiness = {weatherData}
+            // humidity = {weatherData}
+            // windSpeed = {weatherData}
+            />
         </main>
     );
 }
+
