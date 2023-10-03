21f363a9cc9fc3cbcba5841dc417d1bc2fa1b205
kiannaeddy <kme9504@nyu.edu>
2023-10-03 09:15:53 -0400

    added API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,21 @@
-import "../app/global.css"
+import "../app/globals.css" 
 
-export default function Home() {
-  return (
-  <main>
-    <h1>Weather App</h1>
-  </main>
-  )
+export async function getStaticProps() {
+    const res = await fetch (
+        `https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=${process.env.WEATHER_APP_API_KEY}&units=imperial`
+    );
+    const weatherData = await res.json();
+return {
+    props: {
+        weatherData,
+        },
+    };
+}
+export default function Home({weatherData}) {
+    console.log(weatherData);
+    return (
+        <main>
+            <h1>Weather App</h1>
+        </main>
+    );
 }
